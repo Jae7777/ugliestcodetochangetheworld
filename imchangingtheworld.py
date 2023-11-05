7241fa97b96e7292f0faca0862917de4fe0ec9fc
Justin Chen <jchen4086@gmail.com>
2023-11-04 23:15:22 -0400

    provingtheearthisflat

---
+++ imchangingtheworld.py
@@ -0,0 +1,43 @@
+
+theflatearth = [
+"              _-o#&&*''''?d:>b\_",
+'          _o/"`''  '',, dMF9MMMMMHo_,',
+'       .o&#`        `"MbHMMMMMMMMMMMHo.,',
+"     .o"" '         vodM*$&&HMMMMMMMMMM?.",
+"    ,'              $M&ood,~'`(&##MMMMMMH\ ",
+"   /     im        ,MMMMMMM#b?#bobMMMMHMMML                                 `. ___",
+"  &              ?MMMMMMMMMMMMMMMMM7MMM$R*Hk                               __,' __`.                _..----....____",
+" ?$.            :MMMMMflatteningMMMM/HMMM|`*L                  __...--.'``;.   ,.   ;``--..__     .'    ,-._    _.-'",
+"|               |MMMMMMtheworldMMMMMMbMH'   T,           _..-''-------'   `'   `'   `'     O ``-''._   (,;') _,'",
+"$H#:            `*MMMMMMMMMMMMMMMMMMMMb#}'  `?         ,'________________                          \`-._`-','",
+']MMH#             ""*""""*#MMMMMMMMMMMMM     -          `._              ```````````------...___   `-.._`-:',
+"MMMMMb_   chnaging        |MMMMMMMMMMMP'     :             ```--.._      ,.                     ````--...__\-.",
+"HMMMMMMMHo                 `MMMMMMMMMT       .                     `.--. `-`                       ____    |  |`",
+"?MMMMMMMMP    the           9MMMMMMMM}       -                       `. `.         fuel: 83%     ,'`````.  ;  ;`",
+"-?MMMMMMM                  |MMMMMMMMM?,d-    '                         `._`.        __________   `.      \`__/`",
+" :|MMMMMM-                 `MMMMMMMT .M|.   :                             `-:._____/______/___/____`.     \  `",
+"  .9MMM[     world          &MMMMM*' `'    .                                          |       `._    `.    \ ",
+"   :9MMk                    `MMM#'        -                                           `._________`-.   `.   `.___",
+"     &M}                     `          .-                                                          SSt  `------'`",
+"      `&.                             .",
+"        `~,   .                     ./",
+"            . _                  .-",
+"              '`--._,dd###pp=""'",
+]
+
+hasfuel = False
+for flatearth in theflatearth:
+    if flatearth == theflatearth[theflatearth.index(flatearth)]:
+        for earth in flatearth:
+            print(earth, end="")
+    print()
+for thefatearth in theflatearth:
+    def theearthisflat(thefatearthh) :
+        fueltocrashtheearth = [80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95 ,96 ,97, 98, 99, 100]
+        for fueltocreashtheearth in fueltocrashtheearth:
+            if str({fueltocreashtheearth}) + '%' in thefatearthh:
+                return ((True and (False)) or True)
+        return False or (True and False)
+    if theearthisflat(thefatearth):
+        print("crash inbounding")
+    
